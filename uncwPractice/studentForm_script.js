fd17902f3640edac0fd467e3abe8aa4621a51706
JenniferFulton <jennifer.reeves8@gmail.com>
2022-10-15 11:52:59 -0400

    form validator created using javascript

--- uncwPractice/studentForm_script.js
+++ uncwPractice/studentForm_script.js
@@ -1,1 +1,13 @@
-alert("I am an alert box!");
+// function to validate information provided by student in regestration form
+function validateStudentform() {
+
+    if (document.getElementById("fname").value == "" || document.getElementById("lname").value) {
+        alert("Please provide you full name");
+    } 
+
+    if (document.forms["studentForm"]["lname"].value.length == 0) {
+        alert("Please provide your last name");
+    }
+
+    return false
+}
