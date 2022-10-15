9bd8715460e8f9c7b49eeb910e71161135bfb0ef
JenniferFulton <jennifer.reeves8@gmail.com>
2022-10-15 12:03:22 -0400

    form updated with routing to a welcome page

--- uncwPractice/studentForm_script.js
+++ uncwPractice/studentForm_script.js
@@ -1,13 +1,13 @@
 // function to validate information provided by student in regestration form
 function validateStudentform() {
 
-    if (document.getElementById("fname").value == "" || document.getElementById("lname").value) {
+    if (document.getElementById("fname").value == "" || document.getElementById("lname").value == "") {
         alert("Please provide you full name");
+        return false
     } 
 
     if (document.forms["studentForm"]["lname"].value.length == 0) {
         alert("Please provide your last name");
     }
 
-    return false
 }
